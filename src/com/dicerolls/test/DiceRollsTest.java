4087f3d4b94799890b636afb8344d762c65dfbb1
Personal <Personal@Revanth.attlocal.net>
2016-04-23 05:20:33 +0530

    added all test cases

--- src/com/dicerolls/test/DiceRollsTest.java
+++ src/com/dicerolls/test/DiceRollsTest.java
@@ -21,6 +21,19 @@ public class DiceRollsTest {
 		int arr2[] = {1,1,1,2,2};
 		assertEquals(1000,dr.getValues(arr2));
 		
+		int arr3[] = {5,4,5,4,5};
+		assertEquals(580,dr.getValues(arr3));
+		
+		int arr4[] = {4,4,4,4,4};
+		assertEquals(480,dr.getValues(arr4));
 
+		int arr5[] = {1,5,5,5,1};
+		assertEquals(700,dr.getValues(arr5));
+		
+		int arr6[] = {0,0,0,0,0};
+		assertEquals(0,dr.getValues(arr6));
+		
+		int arr7[] = {2,2,2,2,2};
+		assertEquals(200,dr.getValues(arr7));
 	}
 }
