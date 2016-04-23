83338131361d66a1ada0b9d09c66cf4db2308a1f
Personal <Personal@Revanth.attlocal.net>
2016-04-23 05:59:19 +0530

    committing getTripleScore method

--- src/com/dicerolls/main/DiceRolls.java
+++ src/com/dicerolls/main/DiceRolls.java
@@ -6,7 +6,7 @@ import java.util.Set;
 
 public class DiceRolls {
 	private int arr[] = {6,6,6,1,4};
-	
+
 	public int getValues(int[] arr1) {
 		HashMap<Integer, Integer> hMap = new HashMap<Integer, Integer>();
 		for(int i : arr){
@@ -21,25 +21,41 @@ public class DiceRolls {
 	}
 	private int getScore(HashMap<Integer, Integer> hMap) {
 		Set<Entry<Integer, Integer>> entries = hMap.entrySet();
-		
+
 		int score = 0;
 		for(Entry<Integer, Integer> entry : entries ){
 			int count = entry.getValue();
 			if(count >= 3){
 				count = count - 3;
-				
+
 				score += getTripleScore(entry.getKey()); 
-				
+
 			}		
 		}
-			
+
 		return score;
 	}
-	
-	
+
+
 	private int getTripleScore(int key){
 
+		switch(key){
+		case 1:
+			return 1000;
+		case 2:
+			return 200;
+		case 3:
+			return 3000;
+		case 4:
+			return 400;
+		case 5:
+			return 500;
+		case 6:
+			return 600;
+		default:
 			return 0;
-			
+
 		}
+
+	}
 }
