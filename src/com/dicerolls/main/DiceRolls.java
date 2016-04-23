17b4256f355dedc098344699f1ef0d25eeef3f84
Personal <Personal@Revanth.attlocal.net>
2016-04-23 05:38:46 +0530

    intial  method getValues will always return 0

--- src/com/dicerolls/main/DiceRolls.java
+++ src/com/dicerolls/main/DiceRolls.java
@@ -1,9 +1,21 @@
 package com.dicerolls.main;
 
-public class DiceRolls {
+import java.util.HashMap;
 
+public class DiceRolls {
+	private int arr[] = {6,6,6,1,4};
+	
 	public int getValues(int[] arr1) {
+		HashMap<Integer, Integer> hMap = new HashMap<Integer, Integer>();
+		for(int i : arr){
+			int value = 0;
+			if(hMap.containsKey(i)){
+				value = hMap.get(i);
+			}
+			value = value + 1;
+			hMap.put(i, value);
+		}
 		return 0;
 	}
- 
+
 }
